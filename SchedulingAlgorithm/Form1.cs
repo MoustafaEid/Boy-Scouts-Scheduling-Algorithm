467ebfaad14f19911cf532ce2fd051aa2bab6f5d
unknown <Moustafa Eid@MoustafaEid.(none)>
2012-01-28 14:15:09 -0500

    initial

---
+++ SchedulingAlgorithm/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchedulingAlgorithm
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			List<Group> G = new List<Group>();
+
+			G.Add(new Group("Knights 1", 3));
+			G.Add(new Group("Knights 2", 4));
+			G.Add(new Group("Knights 3", 2));
+			G.Add(new Group("Knights 66", 1));
+
+			List<Station> S = new List<Station>();
+
+
+			List<Availability> K = new List<Availability>( new Availability[] 
+							{ 
+								new Availability( 1, new List<int>( new int [] { 1, 3 ,4 } ) ),
+								new Availability( 2, new List<int>( new int [] { 3, 4 } ) ),
+								new Availability( 4, new List<int>( new int [] { 1 } ) ),
+								new Availability( 5, new List<int>( new int [] { 5 } ) ),
+							} );
+
+			S.Add(new Station("Pool", 2, K));
+
+			K = new List<Availability>(new Availability[] 
+							{ 
+								new Availability( 1, new List<int>( new int [] { 1, 3 ,4 } ) ),
+								new Availability( 2, new List<int>( new int [] { 1, 2, 3 ,4 } ) ),
+								new Availability( 3, new List<int>( new int [] { 3, 4,5 } ) ),
+								new Availability( 4, new List<int>( new int [] { 1,4,5 } ) ),
+								new Availability( 5, new List<int>( new int [] { 1,2,3,4,5 } ) ),
+							});
+
+			S.Add(new Station("Archery", 1, K));
+
+			K = new List<Availability>(new Availability[] 
+							{ 
+								new Availability( 1, new List<int>( new int [] { 1, 2, 3 ,4, 5 } ) ),
+								new Availability( 2, new List<int>( new int [] { 1,2,3,4,5 } ) ),
+								new Availability( 3, new List<int>( new int [] { 1,2,3,4,5 } ) ),
+							});
+
+			S.Add(new Station("Shooting", 1, K));
+
+
+			K = new List<Availability>(new Availability[] 
+							{ 
+								new Availability( 2, new List<int>( new int [] { 5 } ) ),
+								new Availability( 4, new List<int>( new int [] { 1 } ) ),
+							});
+
+			S.Add(new Station("Skits", 2, K));
+
+			Dictionary<string, string>[,] A = Scheduler.Schedule(G, S, 5);
+
+			printSchedule(A);
+		}
+
+		private void printSchedule(Dictionary<string, string>[,] masterSchedule)
+		{
+			int i, j, k;
+			richTextBox1.Text = "";
+
+			for (i = 1; i <= 5; i++)
+			{
+				richTextBox1.Text += "Day " + i + "\n----------------------------\n";
+
+				for (j = 1; j <= 5; j++)
+				{
+					richTextBox1.Text += "\t Slot #" + j + "\n\t-----------\n";
+
+					Dictionary<string, string> D = masterSchedule[i, j];
+
+					foreach (KeyValuePair<string, string> P in D)
+					{
+						richTextBox1.Text += "\t\t" + P.Key + " Assigned To " + P.Value + "\n";
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SchedulingAlgorithm/Scheduler.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace SchedulingAlgorithm
+{
+	public class Group
+	{
+		public string Name;
+		public int Rank;
+
+		public Group(string N, int R)
+		{
+			Name = N;
+			Rank = R;
+		}
+	}
+
+	public class Availability
+	{
+		public int DayNumber;
+		public List<int> Slots;
+
+		public Availability(int D, List<int> S)
+		{
+			DayNumber = D;
+			Slots = S;
+		}
+	}
+
+	public class Station
+	{
+		public string Name;
+		public int Capacity;
+		public List<Availability> Avail;
+
+		public Station(string N, int C, List<Availability> A)
+		{
+			Name = N;
+			Capacity = C;
+			Avail = A;
+		}
+	}
+
+	public static class Scheduler
+	{
+		public static Dictionary<string, string>[,] Schedule(List<Group> groups, List<Station> stations, int slotsPerDay)
+		{
+			// start monday end Friday
+			int dayStart = 1, dayEnd = 5;
+			int Day, Slot, i,j, k;
+
+			// [GroupNumber, StationNumber] = Assignment Counter
+			int[,] GroupStationAssignments = new int[100, 100];
+
+			// Schedule
+			Dictionary<string, string>[,] masterSchedule = new Dictionary<string, string>[10, 20];
+
+			for (i = 0; i < GroupStationAssignments.GetLength(0); i++)
+				for (j = 0; j < GroupStationAssignments.GetLength(1); j++)
+					GroupStationAssignments[i, j] = 0;
+
+			for (Day = dayStart; Day <= dayEnd; Day++)
+			{
+				for (Slot = 1; Slot <= slotsPerDay; Slot++)
+				{
+					masterSchedule[Day, Slot] = new Dictionary<string, string>();
+
+					// Group busy
+					bool[] isGroupBusy = new bool[100];
+
+					for (i = 0; i < stations.Count; i++)
+					{
+						Station curStation = stations[i];
+
+						if (!isStationAvailableAtSlot(stations[i], Day, Slot))
+							continue;
+
+						int counter = 1;
+
+						for (j = 0; j < groups.Count; j++)
+						{
+							if (counter > curStation.Capacity)
+								break;
+
+							Group curGroup = groups[j];
+
+							if (GroupStationAssignments[j, i] >= 2 || isGroupBusy[j])
+								continue;
+
+							// Group is busy in this slot
+							isGroupBusy[j] = true;
+							GroupStationAssignments[j, i]++;
+
+							counter++;
+
+							masterSchedule[Day, Slot].Add(curGroup.Name, curStation.Name);
+						}
+					}
+				}
+			}
+
+			return masterSchedule;
+		}
+
+		private static bool isStationAvailableAtSlot(Station station, int Day, int Slot)
+		{
+			int i, j;
+
+			if (station.Capacity <= 0)
+				return false;
+
+			for (i = 0; i < station.Avail.Count; i++)
+			{
+				if (station.Avail[i].DayNumber == Day)
+				{
+					if (station.Avail[i].Slots.IndexOf(Slot) != -1)
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
